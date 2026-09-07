c92196dfdd007652003d991e4cb167af82859cff
user <user@DESKTOP-6HGS7ND>
2026-09-07 21:09:48 +0300

    v.1.1

--- usova_lab0/usova_lab0/usova_lab0.cpp
+++ usova_lab0/usova_lab0/usova_lab0.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{   
+    int x;
+    cin >> x;
+    cout << 10/ x << '\n';
+    cout << "Hello World!\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
